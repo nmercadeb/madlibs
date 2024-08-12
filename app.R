2853eea5e9b60f87be430d35434672c125edaf64
nmercadeb <nmercade147@gmail.com>
2024-08-12 14:06:19 -0700

    add logger

--- app.R
+++ app.R
@@ -1,5 +1,11 @@
 library(shiny)
+library(useself)
 
+cat("LOG --- Get back trace \n", file = stderr())
+rlang::global_entrace()
+options(rlang_backtrace_on_error = "full")
+
+cat("LOG --- Generate story \n", file = stderr())
 generate_story <- function(noun, verb, adjective, adverb) {
   glue::glue("
     Once upon a time, there was a {adjective} {noun} who loved to
@@ -7,6 +13,7 @@ generate_story <- function(noun, verb, adjective, adverb) {
   ")
 }
 
+cat("LOG --- Read ui \n", file = stderr())
 ui <- fluidPage(
   titlePanel("Mad Libs Game"),
   sidebarLayout(
@@ -24,13 +31,17 @@ ui <- fluidPage(
   )
 )
 
+cat("LOG --- Read server \n", file = stderr())
 server <- function(input, output) {
-  story <- eventReactive({
+  cat("LOG --- Reactive story \n", file = stderr())
+  story <- reactive({
     generate_story(input$noun1, input$verb, input$adjective, input$adverb)
   })
+  cat("LOG --- Output story \n", file = stderr())
   output$story <- renderText({
     story()
   })
 }
 
+cat("LOG --- Deploy shiny \n", file = stderr())
 shinyApp(ui = ui, server = server)
